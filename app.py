61939d922174750e627d5b45660846334ae4f219
Trilian <menard.annec@gmail.com>
2021-06-09 14:01:58 +0200

    main dialog

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from bot import MyBot
 from config import DefaultConfig
 from adapter_with_error_handler import AdapterWithErrorHandler
 from flight_booking_recognizer import FlightBookingRecognizer
-from dialogs import BookingDialog
+from dialogs import BookingDialog, MainDialog
 CONFIG = DefaultConfig()
 
 # Create adapter.
@@ -47,7 +47,7 @@ TELEMETRY_CLIENT = ApplicationInsightsTelemetryClient(
 # Create dialogs and Bot
 RECOGNIZER = FlightBookingRecognizer(CONFIG)
 BOOKING_DIALOG = BookingDialog()
-#DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG, telemetry_client=TELEMETRY_CLIENT)
+DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG, telemetry_client=TELEMETRY_CLIENT)
 #BOT = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG, TELEMETRY_CLIENT)
 BOT = MyBot()
 

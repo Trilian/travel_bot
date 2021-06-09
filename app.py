5c4a20403d902df978a4453f9591e9bcd96b78aa
Trilian <menard.annec@gmail.com>
2021-06-09 09:52:32 +0200

    echobot work so test travel_bot

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import sys
-import traceback
 from datetime import datetime
 from http import HTTPStatus
 
@@ -11,8 +8,9 @@ from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
     BotFrameworkAdapterSettings,
-    TurnContext,
-    BotFrameworkAdapter,
+    ConversationState,
+    MemoryStorage,
+    UserState,
 )
 from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
@@ -25,44 +23,33 @@ CONFIG = DefaultConfig()
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
 SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
-ADAPTER = BotFrameworkAdapter(SETTINGS)
 
+# Create MemoryStorage, UserState and ConversationState
+MEMORY = MemoryStorage()
+USER_STATE = UserState(MEMORY)
+CONVERSATION_STATE = ConversationState(MEMORY)
 
-# Catch-all for errors.
-async def on_error(context: TurnContext, error: Exception):
-    # This check writes out errors to console log .vs. app insights.
-    # NOTE: In production environment, you should consider logging this to Azure
-    #       application insights.
-    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
-    traceback.print_exc()
+# Create adapter.
+# See https://aka.ms/about-bot-adapter to learn more about how bots work.
+ADAPTER = AdapterWithErrorHandler(SETTINGS, CONVERSATION_STATE)
 
-    # Send a message to the user
-    await context.send_activity("The bot encountered an error or bug.")
-    await context.send_activity(
-        "To continue to run this bot, please fix the bot source code."
-    )
-    # Send a trace activity if we're talking to the Bot Framework Emulator
-    if context.activity.channel_id == "emulator":
-        # Create a trace activity that contains the error object
-        trace_activity = Activity(
-            label="TurnError",
-            name="on_turn_error Trace",
-            timestamp=datetime.utcnow(),
-            type=ActivityTypes.trace,
-            value=f"{error}",
-            value_type="https://www.botframework.com/schemas/error",
-        )
-        # Send a trace activity, which will be displayed in Bot Framework Emulator
-        await context.send_activity(trace_activity)
+# Create telemetry client.
+# Note the small 'client_queue_size'.  This is for demonstration purposes.  Larger queue sizes
+# result in fewer calls to ApplicationInsights, improving bot performance at the expense of
+# less frequent updates.
+INSTRUMENTATION_KEY = CONFIG.APPINSIGHTS_INSTRUMENTATION_KEY
+TELEMETRY_CLIENT = ApplicationInsightsTelemetryClient(
+    INSTRUMENTATION_KEY, telemetry_processor=AiohttpTelemetryProcessor(), client_queue_size=10
+)
 
-
-ADAPTER.on_turn_error = on_error
-
-# Create the Bot
+# Create dialogs and Bot
+#RECOGNIZER = FlightBookingRecognizer(CONFIG)
+#BOOKING_DIALOG = BookingDialog()
+#DIALOG = MainDialog(RECOGNIZER, BOOKING_DIALOG, telemetry_client=TELEMETRY_CLIENT)
+#BOT = DialogAndWelcomeBot(CONVERSATION_STATE, USER_STATE, DIALOG, TELEMETRY_CLIENT)
 BOT = MyBot()
 
-
-# Listen for incoming requests on /api/messages
+# Listen for incoming requests on /api/messages.
 async def messages(req: Request) -> Response:
     # Main bot message handler.
     if "application/json" in req.headers["Content-Type"]:
@@ -78,10 +65,8 @@ async def messages(req: Request) -> Response:
         return json_response(data=response.body, status=response.status)
     return Response(status=HTTPStatus.OK)
 
-
 def init_func(argv) :
-    #APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
-    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP = web.Application(middlewares=[bot_telemetry_middleware, aiohttp_error_middleware])
     APP.router.add_post("/api/messages", messages)
     return APP
 

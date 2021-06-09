2b7744c2cfa6b3b6dfb754bbabe472f224c2e224
Trilian <menard.annec@gmail.com>
2021-06-09 08:16:36 +0200

    change requierement : pb de compatibilites

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = "f6e44223-91c7-429c-88b2-4669a74182df"
-    APP_PASSWORD = "d72707e7-4331-4253-8f33-682fd6b72199"
+    APP_ID = "9c2af855-06e0-4252-a525-c6a912fc7502"
+    APP_PASSWORD = "da8bdf15-a842-4459-b10f-f1364501f93a"
     LUIS_APP_ID = "cafda911-e530-4237-8983-7452fa0cfe08"
     LUIS_API_KEY = "b8e697c56d0340e5a6df74609c2e5d5e"
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"

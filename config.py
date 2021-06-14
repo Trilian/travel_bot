f5e7e721db9f2e6f05db66e75e42efc597442224
Trilian <menard.annec@gmail.com>
2021-06-14 14:33:04 +0200

    change congi

--- config.py
+++ config.py
@@ -11,10 +11,12 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = "9c2af855-06e0-4252-a525-c6a912fc7502"
-    APP_PASSWORD = "5myP_L_w_uN86cujUW18t1xp1.xcZEnxP-"
+    APP_ID = ""
+    APP_PASSWORD = ""
     LUIS_APP_ID = "5705e735-228d-430c-94fd-cab67b7f69ae"
     LUIS_API_KEY = "b8e697c56d0340e5a6df74609c2e5d5e"
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
     LUIS_API_HOST_NAME = "westeurope.api.cognitive.microsoft.com"
-    APPINSIGHTS_INSTRUMENTATION_KEY = "cfb53cfc-b75a-4d5f-bdc7-db9eb6d86bec"
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
+        "AppInsightsInstrumentationKey", "cfb53cfc-b75a-4d5f-bdc7-db9eb6d86bec"
+    )
